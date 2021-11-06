084056fa0d8cc3a04659ec82e1b036d810be13b8
faiz8411 <faizullah.noa2014@gmail.com>
2021-11-06 20:36:26 +0600

    addservice

--- src/components/AddService/AddService.js
+++ src/components/AddService/AddService.js
@@ -11,7 +11,7 @@ const AddService = () => {
         formState: { errors },
     } = useForm();
     const onSubmit = (data) => {
-        fetch("https://fast-eyrie-50144.herokuapp.com/addServices", {
+        fetch("http://localhost:5000/addServices", {
             method: "POST",
             headers: { "content-type": "application/json" },
             body: JSON.stringify(data),
@@ -19,60 +19,63 @@ const AddService = () => {
             .then((res) => res.json())
             .then((result) => console.log(result));
         console.log(data);
-        reset()
-
-    }
-
-
+    };
     return (
         <div>
-            <h1 className="mt-5 text-center text-info">service added</h1>
-            <div className="">
-                <div className="event-box border border d-flex justify-content-center align-items-center">
-                    <div className="login-form">
-                        <form onSubmit={handleSubmit(onSubmit)}>
-                            <input
-                                {...register("title")}
-                                placeholder="title"
-                                className="p-2 m-2 w-100"
-                            />
-                            <br />
-                            <input
-                                {...register("date")}
-                                // placeholder="Name"
-                                type="date"
-                                className="p-2 m-2 w-100"
-                            />
-                            <br />
-                            <textarea
-                                {...register("description")}
-                                placeholder="Description"
-                                className="p-2 m-2"
-                                className="p-2 m-2 w-100"
-                            />
-                            <br />
+            <div>
+                <h1 className="mt-5 text-center text-info">Please Add Services</h1>
+                <div className="login-box w-25 m-auto mt-5">
+                    <div className="event-box border border d-flex justify-content-center align-items-center">
+                        <div className="login-form">
+                            <form onSubmit={handleSubmit(onSubmit)}>
+                                <input
+                                    {...register("name")}
+                                    placeholder="Name"
+                                    className="p-2 m-2 w-100"
+                                />
+                                <br />
+                                <input
+                                    {...register("date")}
+                                    // placeholder="Name"
+                                    type="date"
+                                    className="p-2 m-2 w-100"
+                                />
+                                <br />
+                                <input
+                                    {...register("description")}
+                                    placeholder="Description"
+                                    className="p-2 m-2"
+                                    className="p-2 m-2 w-100"
+                                />
+                                <br />
 
-                            <input
-                                {...register("image", { required: true })}
-                                placeholder="Image Link"
-                                className="p-2 m-2"
-                                className="p-2 m-2 w-100"
-                            />
-                            <br />
-                            <select {...register("EventColor")} className="p-2 m-2 w-100">
-                                <option value="red">red</option>
-                                <option value="green">green</option>
-                                <option value="blue">blue</option>
-                                <option value="orange">orange</option>
-                                <option value="black">black</option>
-                            </select>
-                            <br />
+                                <input
+                                    {...register("image", { required: true })}
+                                    placeholder="Image Link"
+                                    className="p-2 m-2"
+                                    className="p-2 m-2 w-100"
+                                />
+                                <br />
+                                <input
+                                    {...register("price", { required: true })}
+                                    placeholder="Price"
+                                    type="number"
+                                    className="p-2 m-2"
+                                    className="p-2 m-2 w-100"
+                                />
+                                <br />
 
-                            {errors.exampleRequired && <span>This field is required</span>}
 
-                            <input type="submit" value="Add" className="btn btn-info w-50" />
 
-                        </form>
+                                {errors.exampleRequired && <span>This field is required</span>}
+
+                                <input
+                                    type="submit"
+                                    value="Add"
+                                    className="btn btn-info w-50"
+                                />
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
---
+++ src/images.link.js
@@ -0,0 +1,31 @@
+// title
+// :
+// "castomer care "
+// date
+// :
+// ""
+// description
+// :
+// "https://i.ibb.co/3RSxTqP/customer-care-support-services-500x500-1.jpg"
+// image
+// :
+// "Customized solutions that understand your business type needs"
+// EventColor
+// :
+// "red"
+
+// title
+// :
+// "persel delivery"
+// date
+// :
+// ""
+// description
+// :
+// "First-Mile Pickup and Last Mile Delivery Services for Private, Small B..."
+// image
+// :
+// "https://i.ibb.co/23cGRYW/oarsel.jpg"
+// EventColor
+// :
+// "red"
