c5b93993774312593652e0562c2cefc10f861752
faiz8411 <faizullah.noa2014@gmail.com>
2021-11-27 19:23:31 +0600

    slight change

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -13,23 +13,22 @@ const Services = () => {
 
     }, [])
     return (
-        <div className="p-3">
-            <h1> Services</h1>
+        <div>
+            <h1>Services</h1>
             <div className="services">
                 <div className="row container">
-                    {services?.map((pd) => (
-                        <div className="col-md-4">
-                            <div className="service border border p-3">
-                                <div className="services-img ">
-                                    <img className="w-100" src={pd?.image} alt="" />
+                    {services?.map((pd, index) => (
+                        <div className="col-md-6 col-lg-4 h-50">
+                            <div className="service p-3 border border m-2">
+                                <div className="service-img">
+                                    <img className="img-fluid" src={pd?.image} alt="" />
                                 </div>
+                                <h5>{pd.name}</h5>
+                                <p>{pd.description}</p>
+                                <p>price:{pd.price}</p>
+                                <Link to={`/services/${pd._id}`}>
 
-                                <h6>{pd?.name}</h6>
-                                <h4>{pd?.model}</h4>
-                                <p>{pd?.description}</p>
-                                <h3 className="text-danger"> Cost :{pd?.price}$</h3>
-                                <Link to={`/details/${pd._id}`}>
-                                    <button className="btn btn-success">Add To Cart</button>
+                                    <button className="btn btn-success">Order Now</button>
                                 </Link>
                             </div>
                         </div>
