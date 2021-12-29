6df7d1af1c594a152987e9629cabf24ba0c6e118
faiz8411 <faizullah.noa2014@gmail.com>
2021-12-30 00:02:10 +0600

    slight

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -15,29 +15,35 @@ const Services = () => {
 
     }, [])
     return (
-        <div>
-            <Row xs={1} md={3} className="g-4">
-                {services.map((service, idx) => (
-                    <Service key={service._id}
-                        service={service}
-                    ></Service>
-                    // <Col>
-                    //     <Card>
-                    //         <Card.Img className="w-70" variant="top" src={pd.image} />
-                    //         <Card.Body>
-                    //             <Card.Title>{pd.name}</Card.Title>
-                    //             <Card.Text>
-                    //                 {pd.description}
-                    //             </Card.Text>
-                    //             <Card.Text>
-                    //                 price: {pd.price}
-                    //             </Card.Text>
-                    //             <Link to={`/service/${pd._id}`}><button className="btn btn-success "> details</button></Link>
-                    //         </Card.Body>
-                    //     </Card>
-                    // </Col>
-                ))}
-            </Row>
+        <div className='p-3'>
+            <div className='service-provide'>
+                <h6 className='text'>what we serve</h6>
+                <h1>We Provide Top Destination <br /> specially for you</h1>
+            </div>
+            <div className='service'>
+                <div className="row container">
+
+                    {
+                        services.map(pd => (<div className='col-md-4 col-sm-12'>
+                            <div className="service border border p-3 ">
+                                <div className="images">
+                                    <img className='img-fluid' src={pd.image} alt="" />
+
+                                </div>
+                                <h6 className='description'>{pd.name}</h6>
+
+                                <p className=' description-project'><span className='text-primary'></span> <br /> {pd.description}</p>
+                                <h6 className='price'>price:${pd.price}</h6>
+                                <Link to={`/details/${pd._id}`}><button className='p-2 border-0 bg-primary rounded text-white price'>Booking Now</button></Link>
+                            </div>
+
+                        </div>
+                        ))
+                    }
+
+                </div>
+
+            </div>
         </div>
     );
 };
--- src/components/service/Service.js
+++ src/components/service/Service.js
@@ -6,7 +6,7 @@ const Service = ({ service }) => {
     const { name, image, description, price } = service
     return (
         <div>
-            <Col>
+            {/* <Col>
                 <Card>
                     <Card.Img className="w-70" variant="top" src={service.image} />
                     <Card.Body>
@@ -20,7 +20,7 @@ const Service = ({ service }) => {
                         <Link to={`/service/${service._id}`}><button className="btn btn-success "> details</button></Link>
                     </Card.Body>
                 </Card>
-            </Col>
+            </Col> */}
         </div>
     );
 };
